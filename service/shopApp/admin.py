676379f756e2f0a6e5cd660c0c9f646d7487cc92
D4CHub <akbievalim0578@gmail.com>
2024-04-21 19:17:36 +0300

    Model registration

--- service/shopApp/admin.py
+++ service/shopApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Cart, CartItem, Category, Product
 
-# Register your models here.
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Category)
+admin.site.register(Product)
