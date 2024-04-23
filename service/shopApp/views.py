6542648cde9ef30ab7f9e7a6809686ef658eed89
D4CHub <akbievalim0578@gmail.com>
2024-04-23 20:38:12 +0300

    Add some views ang pagination

--- service/shopApp/views.py
+++ service/shopApp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .models import Category, Product, Cart, CartItem
+from .serializers import ProductSerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class ProductPagination(PageNumberPagination):
+    page_size = 10 
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class ProductAPIView(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = ProductPagination
+
+
+class ProductDetailAPIView(APIView):
+    def get(self, request, pk, format=None):
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
