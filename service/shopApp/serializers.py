361aa923d9588e29f21f742e2fbb40135ba7439a
D4CHub <akbievalim0578@gmail.com>
2024-04-23 20:33:25 +0300

    add ProductSerializer

--- service/shopApp/serializers.py
+++ service/shopApp/serializers.py
@@ -2,33 +2,9 @@ from  rest_framework import serializers
 from .models import Cart, Product, CartItem, Category
 
 
-class BaseModelSerializer(serializers.ModelSerializer):
-    def __init__(self, instance=None, data=..., **kwargs):
-        super().__init__(instance, data, **kwargs)
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField(source='category.name')
 
-    
-        if not hasattr(self.Meta, 'fields'):
-            self.Meta.fields = '__all__'
-
-
-class CategorySerializer(BaseModelSerializer):
-    class Meta:
-        model = Category 
-
- 
-class CartItemSerializer(BaseModelSerializer):
-    class Meta:
-        model = CartItem
-
-
-class CartSerializer(BaseModelSerializer):
-    products = CartItemSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Cart
-        fields = ['user', 'products']
-
-
-class ProductSerializer(BaseModelSerializer):
     class Meta:
         model = Product
+        fields = ['id', 'name', 'regular_price', 'discount_price', 'stock', 'description', 'category']
