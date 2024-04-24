bc0f2db9a095a15ce908daad5e3a29e114fc49ea
D4CHub <akbievalim0578@gmail.com>
2024-04-24 17:24:40 +0300

    Add endpoints

--- service/shopApp/urls.py
+++ service/shopApp/urls.py
@@ -4,8 +4,25 @@ from .views import *
 
 
 urlpatterns = [
-    path('products/', ProductAPIView.as_view({'get': 'list'})),
-    path('products/<int:pk>/', ProductDetailAPIView.as_view()),
+    # Shows all products
+    path('products/', ProductAPIView.as_view({'get': 'list'})), 
+    
+    # Shows the product by id
+    path('products/<int:pk>/', ProductDetailAPIView.as_view()), 
+    
+    # Shows the product category and subcategory 
+    path('categories/<int:id>/products/', ProductByCategoryAPIView.as_view(), name='products-by-category'),
+    
+    # Shows the product stock and regular-discount price
+    path('products/stock/', ProductPriceAPIView.as_view({'get': 'list'})),
+    
+    # A request to add an item to the inventory
+    path('cart/add/', CartAPIView.as_view()),
+    
+    # Request for an order
+    path('order/', OrderAPIView.as_view()),
 
+    #TODO request for an order proucts from user cart
 ]
 
+    
