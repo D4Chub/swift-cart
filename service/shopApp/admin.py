a1aa43475542e273d3da13cacbbd9a8d7f477569
D4CHub <akbievalim0578@gmail.com>
2024-04-24 17:27:23 +0300

    Fix model registration

--- service/shopApp/admin.py
+++ service/shopApp/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
-from .models import Cart, CartItem, Category, Product
+from .models import Cart, CartItem, Category, Product, Order
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
-admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'regular_price', 'discount_price', 'stock']
+    list_display_links = ['id', 'name', 'category']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'parent']
+    list_display_links = ['id', 'name', 'parent']
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user',]
+    list_display_links = ['id', 'user']
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cart', 'product', 'quantity']
+    list_display_links = ['id', 'cart', 'quantity', 'product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'product', 'amount', 'total_price']
+    list_display_links = ['id', 'user', 'product']
+    readonly_fields = ['total_price']
+
+
+
