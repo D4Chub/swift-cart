879a5383d9a8173d65e5f1f983d8b380696172b4
D4CHub <akbievalim0578@gmail.com>
2024-04-24 17:25:35 +0300

    Add serializers for models

--- service/shopApp/serializers.py
+++ service/shopApp/serializers.py
@@ -1,5 +1,5 @@
 from  rest_framework import serializers
-from .models import Cart, Product, CartItem, Category
+from .models import *
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -8,3 +8,24 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'regular_price', 'discount_price', 'stock', 'description', 'category']
+
+
+class ProductPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['name', 'regular_price', 'discount_price', 'stock']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    # product_price = serializers.DecimalField(source='product.price', max_digits=10, decimal_places=2, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+        read_only_fields = ['total_price',]
