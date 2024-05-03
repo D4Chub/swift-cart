607a66a42c3b2b38f3a1b4e19031e7e49f01c70e
D4CHub <akbievalim0578@gmail.com>
2024-05-03 22:11:24 +0300

    fix OrderSerializer

--- service/shopApp/serializers.py
+++ service/shopApp/serializers.py
@@ -27,6 +27,12 @@ class CartSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Cart
-        fields = '__all__'
+        fields = ['user', 'product', 'quantity']
+        read_only_fields = ['total_price']
 
 
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['cart']
+        read_only_fields = ['total_price']
