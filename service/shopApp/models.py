0ba7122205d8cc52c546494e76e7e9e670649ed8
D4CHub <akbievalim0578@gmail.com>
2024-05-03 22:15:18 +0300

    Add total_price field in Cart model

--- service/shopApp/models.py
+++ service/shopApp/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     regular_price = models.DecimalField(max_digits=10, decimal_places=2)
-    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     stock = models.PositiveIntegerField(default=1)
     description = models.TextField(blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
@@ -26,12 +26,15 @@ class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return f'{self.product.name} -- in cart for {self.user.username} [{self.quantity}]'
     
 
-
-
+class Order(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    
 
         
