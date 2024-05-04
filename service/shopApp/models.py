3b74caf24ef27369341bd37e9842dd03a972afee
D4CHub <akbievalim0578@gmail.com>
2024-05-04 21:44:10 +0300

    Add "status" field in Cart model

--- service/shopApp/models.py
+++ service/shopApp/models.py
@@ -27,6 +27,7 @@ class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    status = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.product.name} -- in cart for {self.user.username} [{self.quantity}]'
